fdf0a58632b0a0e2438d8d3e9e58bfbe903f2cdb
中島 瑠伽 <98001@bsp.co.jp>
2018-06-01 14:22:08 +0900

    Z!!!!!!!!

---
+++ CommandLineTest.java
@@ -0,0 +1,10 @@
+package enshu1;
+
+public class CommandLineTest {
+
+    public void main(String[] args) {
+
+        System.out.println("Mr." + args[0] + "'s salary is \\" + args[1] + ".");
+    }
+
+}
